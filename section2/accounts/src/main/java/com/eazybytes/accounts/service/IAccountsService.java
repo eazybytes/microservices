3e56daca7bf1670bafb28b8c6b320453997bd006
eazybytes <tutor@eazybytes.com>
2023-07-10 12:20:28 +0530

    Build Accounts, Loans and Cards microservices

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,35 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Accounts Details based on a given mobileNumber
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
+
+
+}
---
+++ section2/accounts/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `customer` (
+  `customer_id` int AUTO_INCREMENT  PRIMARY KEY,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(20) NOT NULL,
+  `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `customer_id` int NOT NULL,
+   `account_number` int AUTO_INCREMENT  PRIMARY KEY,
+  `account_type` varchar(100) NOT NULL,
+  `branch_address` varchar(200) NOT NULL,
+  `created_at` date NOT NULL,
+   `created_by` varchar(20) NOT NULL,
+   `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
