1a69cb69b60416f1f2147242daff2a25388b88ee
eazybytes <tutor@eazybytes.com>
2023-07-10 18:39:24 +0530

    Build Accounts, Loans and Cards microservices

--- section2/cards/src/main/java/com/eazybytes/cards/constants/CardsConstants.java
+++ section2/cards/src/main/java/com/eazybytes/cards/constants/CardsConstants.java
@@ -7,7 +7,7 @@ public final class CardsConstants {
     }
 
     public static final String  CREDIT_CARD = "Credit Card";
-    public static final int  NEW_CARD_LIMIT = 100000;
+    public static final int  NEW_CARD_LIMIT = 1_00_000;
     public static final String  STATUS_201 = "201";
     public static final String  MESSAGE_201 = "Card created successfully";
     public static final String  STATUS_200 = "200";
