b6f4685e4cfc220c9011afe0012c077856af60db
eazybytes <tutor@eazybytes.com>
2023-07-10 15:24:46 +0530

    Build Accounts, Loans and Cards microservices

---
+++ section2/loans/src/main/java/com/eazybytes/loans/mapper/LoansMapper.java
@@ -0,0 +1,28 @@
+package com.eazybytes.loans.mapper;
+
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.entity.Loans;
+
+public class LoansMapper {
+
+    public static LoansDto mapToLoansDto(Loans loans, LoansDto loansDto) {
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+
+    public static Loans mapToLoans(LoansDto loansDto, Loans loans) {
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+
+}
---
+++ section2/loans/src/main/java/com/eazybytes/loans/service/impl/LoansServiceImpl.java
@@ -0,0 +1,92 @@
+package com.eazybytes.loans.service.impl;
+
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.entity.Loans;
+import com.eazybytes.loans.exception.LoanAlreadyExistsException;
+import com.eazybytes.loans.exception.ResourceNotFoundException;
+import com.eazybytes.loans.mapper.LoansMapper;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.eazybytes.loans.service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class LoansServiceImpl implements ILoansService {
+
+    private LoansRepository loansRepository;
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> optionalLoans= loansRepository.findByMobileNumber(mobileNumber);
+        if(optionalLoans.isPresent()){
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber "+mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new loan details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
+
+    /**
+     *
+     * @param mobileNumber - Input mobile Number
+     * @return Loan Details based on a given mobileNumber
+     */
+    @Override
+    public LoansDto fetchLoan(String mobileNumber) {
+        Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        return LoansMapper.mapToLoansDto(loans, new LoansDto());
+    }
+
+    /**
+     *
+     * @param loansDto - LoansDto Object
+     * @return boolean indicating if the update of loan details is successful or not
+     */
+    @Override
+    public boolean updateLoan(LoansDto loansDto) {
+        Loans loans = loansRepository.findByLoanNumber(loansDto.getLoanNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "LoanNumber", loansDto.getLoanNumber()));
+        LoansMapper.mapToLoans(loansDto, loans);
+        loansRepository.save(loans);
+        return  true;
+    }
+
+    /**
+     * @param mobileNumber - Input MobileNumber
+     * @return boolean indicating if the delete of loan details is successful or not
+     */
+    @Override
+    public boolean deleteLoan(String mobileNumber) {
+        Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        loansRepository.deleteById(loans.getLoanId());
+        return true;
+    }
+
+
+}
