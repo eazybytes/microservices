7db4b9a21c084ae6da95494ed2025d5723316d00
eazybytes <tutor@eazybytes.com>
2024-01-03 19:19:31 +0530

    Configuration management inside microservices

--- section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -1,24 +1,29 @@
 package com.eazybytes.accounts.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
-public class Customer extends  BaseEntity {
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native",strategy = "native")
-    @Column(name="customer_id")
+    @GeneratedValue
+    @Column(name = "customer_id")
     private Long customerId;
 
     private String name;
 
     private String email;
 
-    @Column(name="mobile_number")
+    @Column(name = "mobile_number")
     private String mobileNumber;
 
 }
