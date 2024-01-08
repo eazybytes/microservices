449fd8cdf63bc9b6b735394862fa2a08168226ff
eazybytes <tutor@eazybytes.com>
2024-01-08 14:13:55 +0530

    Using MySQL DBs inside microservices

--- section7/loans/src/main/java/com/eazybytes/loans/entity/Loans.java
+++ section7/loans/src/main/java/com/eazybytes/loans/entity/Loans.java
@@ -1,28 +1,33 @@
 package com.eazybytes.loans.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Loans extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-	@GenericGenerator(name = "native",strategy = "native")
-	private Long loanId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
 
-	private String mobileNumber;
+    private String mobileNumber;
 
-	private String loanNumber;
+    private String loanNumber;
 
-	private String loanType;
+    private String loanType;
 
-	private int totalLoan;
+    private int totalLoan;
 
-	private int amountPaid;
+    private int amountPaid;
 
-	private int outstandingAmount;
-	
+    private int outstandingAmount;
+
 }
